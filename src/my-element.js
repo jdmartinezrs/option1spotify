711741ec9bd9f3e45e243d8ea481d5cc4aace91a
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-05-10 15:57:20 -0500

    feat: :sparkles: creado el grid y el componente de la barra lateral izquierda

--- src/my-element.js
+++ src/my-element.js
@@ -1,131 +1,195 @@
 import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
+import infinitLogo from "/mastercard-line.svg"
+import userLogo from "/Ellipse 13.svg"
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export class MyElement extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
-
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
-    }
-  }
+  
 
   constructor() {
-    super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+    super();
   }
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
+      <div class = "container">
+        <nav class = "leftBar">
+          <my-left-bar></my-left-bar>
+        </nav>
+        <div class = "leftSection"></div>
+        <div class = "middleSection">Hello section</div>
+        <div class = "rightSection"></div>
       </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
     `
-  }
-
-  _onClick() {
-    this.count++
   }
 
   static get styles() {
     return css`
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
+      .container{
+        display: grid;
+        grid-template-columns: .2fr 1.5fr 2fr 1.5fr;
+        height: 90vh; 
+      }
+      .leftBar{
+        grid-column:1/2;
+        background: red;
+      }
+      .leftSection{
+        grid-column:2/3;
+        background: blue;
+      }
+      .middleSection{
+        grid-column: 3/4;
+        background: yellow;
+      }
+      .rightSection{
+        grid-column: 4/5;
+        background: violet;
       }
 
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
+      @media (max-width: 750px) {
+        .container{
+          grid-template-columns: 1fr;
         }
-        button {
-          background-color: #f9f9f9;
+        .middleSection{
+          grid-column:1/2;
+          background: yellow;
+        }
+        .leftBar{
+          display: none;
+        }
+        .leftSection{
+          display: none;
+        }
+        .rightSection{
+          display: none;
         }
       }
     `
   }
 }
 
-window.customElements.define('my-element', MyElement)
+customElements.define('my-element', MyElement)
+
+export class MyLeftBar extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class="leftBar__user">
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${userLogo}>
+        </a>
+      </div>
+      <hr>
+      <div class="leftBar__options">
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      *{
+        display:flex;
+        align-items: center;
+        justify-content: center; 
+      }
+      img{
+        width: 2.5em;
+        height: auto;
+      }
+      .leftBar__user{
+        display: flex;
+        flex-direction: column;
+        margin: 2em 0;
+      }
+      .leftBar__options{
+        display: flex;
+        flex-direction: column;
+        margin-top: 2em;
+        gap: 1em
+      }
+    `
+  }
+}
+
+customElements.define('my-left-bar', MyLeftBar); 
+
+export class MyLeftBar extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class="leftBar__user">
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${userLogo}>
+        </a>
+      </div>
+      <hr>
+      <div class="leftBar__options">
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      *{
+        display:flex;
+        align-items: center;
+        justify-content: center; 
+      }
+      img{
+        width: 2.5em;
+        height: auto;
+      }
+      .leftBar__user{
+        display: flex;
+        flex-direction: column;
+        margin: 2em 0;
+      }
+      .leftBar__options{
+        display: flex;
+        flex-direction: column;
+        margin-top: 2em;
+        gap: 1em
+      }
+    `
+  }
+}
+
+customElements.define('my-left-bar', MyLeftBar); 
