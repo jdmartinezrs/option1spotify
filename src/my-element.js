88977271a2b04075c5d5098e960e987f071fc526
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-05-10 17:04:28 -0500

    feat: :sparkles: Web Components de las secciones realizado

--- src/my-element.js
+++ src/my-element.js
@@ -15,9 +15,15 @@ export class MyElement extends LitElement {
         <nav class = "leftBar">
           <my-left-bar></my-left-bar>
         </nav>
-        <div class = "leftSection"></div>
-        <div class = "middleSection">Hello section</div>
-        <div class = "rightSection"></div>
+        <div class = "leftSection">
+          <my-left-section></my-left-section>
+        </div>
+        <div class = "middleSection">
+          <my-middle-section></my-middle-section>
+        </div>
+        <div class = "rightSection">
+          <my-right-section></my-right-section>
+        </div>
       </div>
     `
   }
@@ -101,46 +107,7 @@ export class MyLeftBar extends LitElement {
           <img src=${infinitLogo}>
         </a>
       </div>
-    `
-  }
-
-  static get styles() {
-    return css`
-      *{
-        display:flex;
-        align-items: center;
-        justify-content: center; 
-      }
-      img{
-        width: 2.5em;
-        height: auto;
-      }
-      .leftBar__user{
-        display: flex;
-        flex-direction: column;
-        margin: 2em 0;
-      }
-      .leftBar__options{
-        display: flex;
-        flex-direction: column;
-        margin-top: 2em;
-        gap: 1em
-      }
-    `
-  }
-}
-
-customElements.define('my-left-bar', MyLeftBar); 
-
-export class MyLeftBar extends LitElement {
-  
-  constructor() {
-    super();
-  }
-
-  render() {
-    return html`
-      <div class="leftBar__user">
+      <div class="leftBar__exit">
         <a href="#">
           <img src=${infinitLogo}>
         </a>
@@ -148,28 +115,14 @@ export class MyLeftBar extends LitElement {
           <img src=${userLogo}>
         </a>
       </div>
-      <hr>
-      <div class="leftBar__options">
-        <a href="#">
-          <img src=${infinitLogo}>
-        </a>
-        <a href="#">
-          <img src=${infinitLogo}>
-        </a>
-        <a href="#">
-          <img src=${infinitLogo}>
-        </a>
-        <a href="#">
-          <img src=${infinitLogo}>
-        </a>
-      </div>
     `
   }
 
   static get styles() {
     return css`
-      *{
+      :host{
         display:flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center; 
       }
@@ -188,8 +141,187 @@ export class MyLeftBar extends LitElement {
         margin-top: 2em;
         gap: 1em
       }
+      .leftBar__exit{
+        position: absolute;
+        bottom: 5em;
+        display: flex;
+        flex-direction: column;
+      }
     `
   }
 }
 
 customElements.define('my-left-bar', MyLeftBar); 
+
+export class MyLeftSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class="leftSection__title"></div>
+      <div class="leftSection__topChart"></div>
+      <div class="leftSection__mayLike"></div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      :host{
+        display: grid;
+        grid-rows: .5fr, 5fr, 5fr;
+        height: 90vh;
+        gap: 1em;
+        padding: 1em;
+      }
+      .leftSection__title{
+        margin-top: 1em;
+        grid-rows: 1/2;
+        background: black;
+        max-height: 100px;
+      }
+      .leftSection__topChart{
+        grid-rows: 2/3;
+        background: green;
+        min-height: 250px;
+      }
+      .leftSection__mayLike{
+        grid-rows: 3/4;
+        background: brown;
+        min-height: 300px;
+      }
+    `
+  }
+}
+
+customElements.define('my-left-section', MyLeftSection); 
+
+
+export class MyMiddleSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class = middleSection__container>
+        <div class = "middleSection__title"></div>
+        <div class = "middleSection__image"></div>
+        <div class = "middleSection__songTitle"></div>
+        <div class = "middleSection__barDuration"></div>
+        <div class = "middleSection__bottons"></div>
+        <div class = "middleSection__barVolume"></div>
+        <div class = "middleSection__deviceReference"></div>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .middleSection__container{
+        display:grid;
+        grid-template-rows: .2fr, 2fr, .35fr, .15fr, .45fr, .12fr, .2fr;
+        gap: .5em;
+        height: 90vh;
+        padding: 1em;
+      }
+      .middleSection__title{
+        grid-rows: 1/2;
+        background: white;
+        max-height: 40px;
+      }
+      .middleSection__image{
+        grid-rows: 2/3;
+        background: white;
+        min-height: 300px;
+      }
+      .middleSection__songTitle{
+        grid-rows: 3/4;
+        background: white;
+        
+      }
+      .middleSection__barDuration{
+        grid-rows: 4/5;
+        background: white;
+        max-height: 25px;
+        display: flex;
+        align-items:center;
+        justify-content: center;
+      }
+      .middleSection__bottons{
+        grid-rows: 5/6;
+        background: white;
+        min-height: 30px;
+      }
+      .middleSection__barVolume{
+        grid-rows: 6/7;
+        background: white;
+        max-height: 20px;
+      }
+      .middleSection__deviceReference{
+        grid-rows: 7/8;
+        background: white;
+        max-height: 40px;
+      }
+    `
+  }
+}
+
+customElements.define('my-middle-section', MyMiddleSection); 
+
+
+export class MyRightSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class = rightSection__container>
+        <div class = "rightSection__title"></div>
+        <div class = "rightSection__tools"></div>
+        <div class = "rightSection__playingNext"></div>
+        <div class = "rightSection__Songs"></div>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .rightSection__container{
+        display: grid;
+        grid-template-rows: .2fr, .2fr, .15fr, 1fr;
+        height: 90vh;
+        padding: 1em;
+        gap: .1em;
+      }
+      .rightSection__title{
+        grid-rows:1/2;
+        background: white;
+        max-height: 40px;
+      }
+      .rightSection__tools{
+        grid-rows:2/3;
+        background: white;
+        max-height: 40px;
+      }
+      .rightSection__playingNext{
+        grid-rows:3/4;
+        background: white;
+        max-height: 20px;
+      }
+      .rightSection__Songs{
+        grid-rows:4/5;
+        background: white;
+        min-height: 620px 
+      }
+    `
+  }
+}
+
+customElements.define('my-right-section', MyRightSection); 
+
