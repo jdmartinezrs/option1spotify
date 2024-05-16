fe20c0220d2fc64a67e2789c17e31131ee73de32
jdmartinezrs <azrojas123@gmail.com>
2024-05-16 02:43:00 -0500

    feat: :sparkles: The center component was included, this component was solved with an album

--- src/my-spotify.js
+++ src/my-spotify.js
@@ -1,47 +1,18 @@
 import { LitElement,html,css } from "lit";
 class mySpotify extends LitElement{
-    static properties= {
-        track: {type:String}
-    }
-    static styles=  css `
+    static styles = css `
     .track{
-      width:100%;
-      height: 500px; 
+        width: 100%;
+        height: 500px;
     }
-
-    `; 
-      
-
-
-        // window.onSpotifyIframeApiReady=(IFrameAPI)=>{const element = document.getElementById('embed-iframe');
-        // const options = {
-        //     uri: 'spotify:episode:7makk4oTQe1546B0PZlDM5'
-        // };
-        // const callback = (EmbedController) =>{};
-        // IFrameAPI.createController(element, options, callback);
-        // };
-    render(){
-       return html`
-       ${this.track}
     
-     `
-    } 
-    async getTrack(){
-        const config ={
-            method: 'GET',
-            headers: {
-                'X-RapidApI-Key': '3f4c660f71msh5286a7e33f1ba8fp1428c3jsnc1252ecd5a6d',
-                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-            }
-
-        };
-        let res = await fetch("https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv",config)
-        let data = await res.json();
-        
-    }
-connectedCallback(){
-    this.getTrack();yy
-}
+    `;
     
-}
-customElements.define("my-spotify",mySpotify)
+ render(){
+    return html`<iframe 
+    class = "track"
+    style="border-radius:12px" src="https://open.spotify.com/embed/album/3IBcauSj5M2A6lTeffJzdv"  frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+ }
+ 
+} 
+customElements.define("my-spotify", mySpotify)
